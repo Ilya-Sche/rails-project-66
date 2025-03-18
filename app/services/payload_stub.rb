ebc05a29254970b93fff922e9b17663280483fd5
Илья <ilya@1ilya_rostov_2004@mail.ru>
2025-03-18 12:36:05 +0300

    Test

--- app/services/payload_stub.rb
+++ app/services/payload_stub.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class PayloadStub
+  def initialize(repository_id, repository_full_name)
+    @repository_id = repository_id
+    @repository_full_name = repository_full_name
+  end
+
   def payload
     {
-      'repository' => { 'full_name' => 'test_user/test_repo', 'id' => '123' },
+      'repository' => { 'id' => @repository_id, 'full_name' => @repository_full_name },
       'pusher' => { 'email' => 'test@example.com' },
       'commits' => [{ 'sha' => 'fake_commit_sha' }]
     }.to_json
